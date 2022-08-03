f6559b41e6715a251932b0cc32f626b44dff389c
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-03 09:58:50 -0500

    Se implementa el Header Card para la page index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
 
 const Index = () => (
     <Layout>
-        <h1>Hello World</h1>
+        {/* Header Card */}
+        <header className="row">
+            <div className="col-md-12">
+                <div className="card car-body bg-dark text-light">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img src="/ryan-ray-profile2.jpeg" className="img-fluid" />
+                        </div>
+                        <div className="col-md-8">
+                            <h1>Rayn Ray</h1>
+                            <h3>FullStack Developer</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae hic accusamus fugiat sequi ipsa necessitatibus quisquam, quaerat vel? Ducimus quod unde perspiciatis numquam sunt nesciunt, placeat adipisci consectetur iusto minima.</p>
+                            <Link href={ '/hireMe' }>
+                                <a>Hire Me</a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     </Layout>
 )
 
