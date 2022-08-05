42c3649aa92c63edb9872d6b29deb1d787a612ed
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-05 11:31:28 -0500

    Se hace consumo de servicio para obtener la info de nuestra cuenta de GitHub mediante el metodo getServerSideProps

--- pages/github.jsx
+++ pages/github.jsx
@@ -1,9 +1,33 @@
 import Layout from "../components/Layout";
 
-const GitHub = () => (
-    <Layout>
-        <h1>GitHub</h1>
-    </Layout>
-)
+const GitHub = (props) => {
+    return (
+        <Layout>
+            <div className="row">
+                <div className="col-md-4 offset-md-4">
+                    <div className="card card-body text-center">
+                        <h1>{ props.user.name }</h1>
+                        <img src={ props.user.avatar_url } />
+                        <p>{ props.user.bio }</p>
+                        <a href={ props.user.blog} target="_blank" className="btn btn-info my-2"> My Blog </a>
+                        <a href={ props.user.html_url} target="_blank" className="btn btn-outline-info"> Go to GitHub </a>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )    
+}
+
+export async function getServerSideProps() {
+    const res= await fetch('https://api.github.com/users/maquinoBSC');
+    const data = await res.json();
+    console.log(data);
+
+    return {
+        props: {
+            user: data
+        }
+    }
+}
 
 export default GitHub;
