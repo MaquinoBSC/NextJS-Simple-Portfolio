aad7f2ec67b18c2c8a7c151b3d3da5110a79188e
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-04 19:39:57 -0500

    Se agrega el Footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,6 +6,13 @@ const Layout = ({ children }) => (
         <main className="container py-4">
             { children }
         </main>
+        <footer className="bg-dark text-light text-center">
+            <div className="container p-4">
+                <h1>&copy; Maquino Hernandez Portfolio</h1>
+                <p>2000 - { new Date().getFullYear() }</p>
+                <p>All right Reserved</p>
+            </div>
+        </footer>
     </>
 )
 
